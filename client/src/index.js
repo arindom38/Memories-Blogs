09520d29cac307e1d77bfac574e740c00b369e00
Arindom Kundu <arindomkundu18@gmail.com>
2021-07-03 13:01:32 +0600

    added redux store and provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk" //middle ware for return action as function
+import combineReducers from "./reducers/rootRd"
+//creating a rdux store that hold global state of app, arguments : reducer, actions
+const store = createStore(combineReducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
 
