d36bcfccb0a256ff5cc29a15a9def9b3a8c69180
Arindom Kundu <arindomkundu18@gmail.com>
2021-07-07 11:26:31 +0600

    removed unrelated files

--- client/src/api/apiCall.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-const url = "https://memories-create-app.herokuapp.com/blogs"
-
-export const fetchBlogs = () => axios.get(url)
-export const createBlog = (newBlog) => axios.post(url,newBlog)
-export const updateBlog = (id,updateBlog) => axios.patch(`${url}/${id}`,updateBlog)
-export const deleteBlog = (id) => axios.delete(`${url}/${id}`)
-export const likeBlog = (id) => axios.patch(`${url}/${id}/likeBlog`)
--- client/src/reducers/rootRd.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from "redux"
-import blogs from "./blogsRd"
-
-export default combineReducers({
-    blogs,
-})
