d19e693216acdd6eb17d21327f8377fe2c519e1c
Arindom Kundu <arindomkundu18@gmail.com>
2021-07-10 02:13:48 +0600

    some fixation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import { Container, Grow, Grid } from "@material-ui/core"
+import { Container} from "@material-ui/core"
 import Navbar from "./Components/Navbar/navbar"
 import Home from "./Components/Home/home"
+import Auth from "./Components/Auth/auth"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
         </Switch>
       </Container>
     </BrowserRouter>
