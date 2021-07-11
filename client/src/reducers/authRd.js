4e5efd23047f56fa6a7a0f417437f5558db8ea8f
Arindom Kundu <arindomkundu18@gmail.com>
2021-07-11 11:07:39 +0600

    token deletion after logout

--- client/src/reducers/authRd.js
+++ client/src/reducers/authRd.js
@@ -1,15 +1,18 @@
-import {AUTH,LOGOUT} from "../constants/actionTypes"
+import { AUTH, LOGOUT } from "../constants/actionTypes"
 
 //reducer must maintain the parameter first state , then action
-const authRd = (state = {authData:null},action) => {
+const authRd = (state = { authData: null }, action) => {
     switch (action.type) {
         case AUTH:
-           //save auth data in loca sotage so browser in client can know log in
-           localStorage.setItem('profile',JSON.stringify({...action?.data}))
-            return {...state,authData: action?.data};
+            //save auth data in loca sotage so browser in client can know log in
+            localStorage.setItem('profile', JSON.stringify({ ...action?.data }))
+            return { ...state, authData: action?.data };
+        case LOGOUT:
+            localStorage.clear()
+            return { ...state, authData: null }
         default:
             return state;
     }
 }
- 
+
 export default authRd;
