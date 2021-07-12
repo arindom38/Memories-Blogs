ef63612ff3a1d0eaa76ba5c38a74be942f198307
Arindom Kundu <arindomkundu18@gmail.com>
2021-07-12 10:41:26 +0600

    Sign in sign up fixation

--- server/Controllers/usersCtl.js
+++ server/Controllers/usersCtl.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose"
 import Users from "../Modules/Users.js"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
@@ -6,7 +5,7 @@ import jwt from "jsonwebtoken"
 export const signIn = async (req,res) =>{
     const {email,password} = req.body
     try {
-        const existingUser = await Users.findOne(email)
+        const existingUser = await Users.findOne({email})
         
         if(!existingUser) return res.status(404).json({message: "User doesn't exist"})
 
@@ -24,23 +23,22 @@ export const signIn = async (req,res) =>{
 
 export const signUp = async (req,res) =>{
     const {firstName,lastName,email,password,confirmPassword} = req.body
-
     try {
-        const existingUser = await Users.findOne(email)
+        const existingUser = await Users.findOne({email})
         
         if(existingUser) return res.status(400).json({message: "User Already exist"})
 
         //check password with confirmpassword
         if(password !== confirmPassword) return res.status(400).json({message: "Password doesn't match please try again"})
 
-        const hashedPassword = await bcrypt.hash(password,12) //hash(pass, difficulty level)
-
-        const newUser = await Users.create({email,name:`${firstName} ${lastName}`,password:hashedPassword})
+       const hashedPassword = await bcrypt.hash(password,12) //hash(pass, difficulty level)
+        
+        const newUser = await Users.create({email:email,name:`${firstName} ${lastName}`,password:hashedPassword})
 
         const token = jwt.sign({email:newUser.email, id: newUser._id},'test',{expiresIn: '1h'})
-
+        
         res.status(200).json({result:newUser,token})
     } catch (error) {
-        res.status(500).json({message: "something went wrong"})
+        res.status(500).json({message: "something went wrong in sign Up"})
     }
 }
